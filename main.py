f73018fa3d2653b86b2479c0b5b3546282e1bf6d
MeChamp1kА <mechamp28@gmail.com>
2025-07-07 18:33:21 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+win = pygame.display.set_mode((700, 500))  # Исправлено: добавлены скобки для кортежа
+clock = pygame.time.Clock()
+
+win.fill((0, 220, 255))  # Исправлено: добавлены скобки для кортежа цвета
+
+is_game = True
+
+while is_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # Исправлено: добавлен отступ
+            is_game = False
+    pygame.display.update()
+    clock.tick(40)
