19a8312087bd1cbafa14fda41488b835402b7179
MeChamp1kА <mechamp28@gmail.com>
2025-07-09 18:24:22 +0300

    two commit

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ clock = pygame.time.Clock()
 
 win.fill((0, 220, 255))  # Исправлено: добавлены скобки для кортежа цвета
 
+
+class GameSprite(pygame.sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = pygame.transform.scale(pygame.image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+
+
+
 is_game = True
-#阿萨的官方太热
+
+
 while is_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # Исправлено: добавлен отступ
